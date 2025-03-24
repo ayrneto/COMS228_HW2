173249420769b132e2188a6c67f4905c4c67753d
ayrneto <ayrneto@gmail.com>
2025-03-23 23:55:36 -0500

    03/23/2025

---
+++ src/main/java/edu/iastate/cs2280/hw2/InsertionSorter.java
@@ -0,0 +1,55 @@
+package edu.iastate.cs2280.hw2;
+
+import java.io.FileNotFoundException;
+import java.lang.NumberFormatException; 
+import java.lang.IllegalArgumentException; 
+import java.util.InputMismatchException;
+
+
+/**
+ *  
+ * @author Ayr Nasser Neto
+ *
+ */
+
+/**
+ * 
+ * This class implements insertion sort.   
+ *
+ */
+
+public class InsertionSorter extends AbstractSorter 
+{
+	// Other private instance variables if you need ... 
+	
+	/**
+	 * Constructor takes an array of points.  It invokes the superclass constructor, and also 
+	 * set the instance variables algorithm in the superclass.
+	 * 
+	 * @param pts  
+	 */
+	public InsertionSorter(Point[] pts) 
+	{
+		super(pts);
+		super.algorithm = "insertion sort";
+	}	
+
+	
+	/** 
+	 * Perform insertion sort on the array points[] of the parent class AbstractSorter.  
+	 */
+	@Override 
+	public void sort()
+	{
+		for(int i = 1; i < points.length; i++){
+			Point current = points[i];
+			int k = i - 1;
+
+			while(k >= 0 && pointComparator.compare(points[k], current) > 0){
+				points[k+1] = points[k];
+				k--;
+			}
+			points[k+1] = current;
+		}
+	}		
+}
---
+++ src/main/java/edu/iastate/cs2280/hw2/SelectionSorter.java
@@ -0,0 +1,61 @@
+package edu.iastate.cs2280.hw2;
+
+import java.io.FileNotFoundException;
+import java.lang.NumberFormatException; 
+import java.lang.IllegalArgumentException; 
+import java.util.InputMismatchException;
+
+
+/**
+ *  
+ * @author Ayr Nasser Neto
+ *
+ */
+
+/**
+ * 
+ * This class implements selection sort.   
+ *
+ */
+
+public class SelectionSorter extends AbstractSorter
+{
+	// Other private instance variables if you need ... 
+	
+	/**
+	 * Constructor takes an array of points.  It invokes the superclass constructor, and also 
+	 * set the instance variables algorithm in the superclass.
+	 *  
+	 * @param pts  
+	 */
+	public SelectionSorter(Point[] pts)  
+	{
+		super(pts);
+		super.algorithm = "selection sort";
+	}	
+
+	
+	/** 
+	 * Apply selection sort on the array points[] of the parent class AbstractSorter.  
+	 * 
+	 */
+	@Override 
+	public void sort()
+	{
+		int minIndex = 0;
+		Point temp = null;
+
+		for(int i = 0; i < (points.length - 1); i++){
+			minIndex = i;
+			for(int k = (i+1); k < points.length; k++){
+				// TODO: I think i call setComparator here, and then inside the if() check the pointComparator value
+				if(pointComparator.compare(points[k], points[minIndex]) < 1){
+					minIndex = k;
+				}
+			}
+			temp = points[minIndex];
+			points[minIndex] = points[i];
+			points[i] = temp;
+		}
+	}	
+}
