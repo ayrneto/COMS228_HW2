7899b0e564679c45a04f3006704fe70736234ace
ayrneto <61766742+ayrneto@users.noreply.github.com>
2025-03-24 16:13:39 -0500

    Update InsertionSorter.java

--- src/main/java/edu/iastate/cs2280/hw2/InsertionSorter.java
+++ src/main/java/edu/iastate/cs2280/hw2/InsertionSorter.java
@@ -1,55 +1,55 @@
 package edu.iastate.cs2280.hw2;
 
 import java.io.FileNotFoundException;
-import java.lang.NumberFormatException; 
-import java.lang.IllegalArgumentException; 
+import java.lang.NumberFormatException;
+import java.lang.IllegalArgumentException;
 import java.util.InputMismatchException;
 
 
 /**
- *  
+ *
  * @author Ayr Nasser Neto
  *
  */
 
 /**
- * 
- * This class implements insertion sort.   
+ *
+ * This class implements insertion sort.
  *
  */
 
-public class InsertionSorter extends AbstractSorter 
+public class InsertionSorter extends AbstractSorter
 {
-	// Other private instance variables if you need ... 
-	
-	/**
-	 * Constructor takes an array of points.  It invokes the superclass constructor, and also 
-	 * set the instance variables algorithm in the superclass.
-	 * 
-	 * @param pts  
-	 */
-	public InsertionSorter(Point[] pts) 
-	{
-		super(pts);
-		super.algorithm = "insertion sort";
-	}	
+    // Other private instance variables if you need ...
 
-	
-	/** 
-	 * Perform insertion sort on the array points[] of the parent class AbstractSorter.  
-	 */
-	@Override 
-	public void sort()
-	{
-		for(int i = 1; i < points.length; i++){
-			Point current = points[i];
-			int k = i - 1;
+    /**
+     * Constructor takes an array of points.  It invokes the superclass constructor, and also
+     * set the instance variables algorithm in the superclass.
+     *
+     * @param pts
+     */
+    public InsertionSorter(Point[] pts)
+    {
+        super(pts);
+        super.algorithm = "insertion sort";
+    }
 
-			while(k >= 0 && pointComparator.compare(points[k], current) > 0){
-				points[k+1] = points[k];
-				k--;
-			}
-			points[k+1] = current;
-		}
-	}		
+
+    /**
+     * Perform insertion sort on the array points[] of the parent class AbstractSorter.
+     */
+    @Override
+    public void sort()
+    {
+        for(int i = 1; i < points.length; i++){
+            Point current = points[i];
+            int k = i - 1;
+
+            while(k >= 0 && pointComparator.compare(points[k], current) > 0){
+                points[k+1] = points[k];
+                k--;
+            }
+            points[k+1] = current;
+        }
+    }
 }
