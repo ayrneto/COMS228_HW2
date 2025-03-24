b8cb6a09cc1b39d7ac86ee07269ef6d87283b542
ayrneto <61766742+ayrneto@users.noreply.github.com>
2025-03-24 16:14:46 -0500

    Update QuickSorter.java

--- src/main/java/edu/iastate/cs2280/hw2/QuickSorter.java
+++ src/main/java/edu/iastate/cs2280/hw2/QuickSorter.java
@@ -1,96 +1,96 @@
 package edu.iastate.cs2280.hw2;
 
 import java.io.FileNotFoundException;
-import java.lang.NumberFormatException; 
-import java.lang.IllegalArgumentException; 
+import java.lang.NumberFormatException;
+import java.lang.IllegalArgumentException;
 import java.util.InputMismatchException;
 
 
 /**
- *  
+ *
  * @author Ayr Nasser Neto
  *
  */
 
 /**
- * 
- * This class implements the version of the quicksort algorithm presented in the lecture.   
+ *
+ * This class implements the version of the quicksort algorithm presented in the lecture.
  *
  */
 
 public class QuickSorter extends AbstractSorter
 {
-	
-	// Other private instance variables if you need ... 
-		
-	/** 
-	 * Constructor takes an array of points.  It invokes the superclass constructor, and also 
-	 * set the instance variables algorithm in the superclass.
-	 *   
-	 * @param pts   input array of integers
-	 */
-	public QuickSorter(Point[] pts)
-	{
-		super(pts);
-		super.algorithm = "quicksort";
-	}
-		
 
-	/**
-	 * Carry out quicksort on the array points[] of the AbstractSorter class.  
-	 * 
-	 */
-	@Override 
-	public void sort()
-	{
-		quickSortRec(0, points.length - 1);
-	}
-	
-	
-	/**
-	 * Operates on the subarray of points[] with indices between first and last. 
-	 * 
-	 * @param first  starting index of the subarray
-	 * @param last   ending index of the subarray
-	 */
-	private void quickSortRec(int first, int last)
-	{
-		if(first >= last){
-			return;
-		}
+    // Other private instance variables if you need ...
 
-		int pivotIndex = partition(first, last);
-
-		quickSortRec(first, pivotIndex - 1);
-		quickSortRec(pivotIndex + 1, last);
-	}
-	
-	
-	/**
-	 * Operates on the subarray of points[] with indices between first and last.
-	 * 
-	 * @param first
-	 * @param last
-	 * @return
-	 */
-	private int partition(int first, int last)
-	{
-		Point pivot = points[last];
-		int i = first - 1;
-
-		for(int j = first; j < last; j++){
-			if(pointComparator.compare(points[j], pivot) <= 0){
-				i++;
-				swap(j,i);
-			}
-		}
-
-		swap(i + 1, last);
-		return i + 1;
-	}	
-		
+    /**
+     * Constructor takes an array of points.  It invokes the superclass constructor, and also
+     * set the instance variables algorithm in the superclass.
+     *
+     * @param pts   input array of integers
+     */
+    public QuickSorter(Point[] pts)
+    {
+        super(pts);
+        super.algorithm = "quicksort";
+    }
 
 
-	
-	// Other private methods if needed ...
+    /**
+     * Carry out quicksort on the array points[] of the AbstractSorter class.
+     *
+     */
+    @Override
+    public void sort()
+    {
+        quickSortRec(0, points.length - 1);
+    }
+
+
+    /**
+     * Operates on the subarray of points[] with indices between first and last.
+     *
+     * @param first  starting index of the subarray
+     * @param last   ending index of the subarray
+     */
+    private void quickSortRec(int first, int last)
+    {
+        if(first >= last){
+            return;
+        }
+
+        int pivotIndex = partition(first, last);
+
+        quickSortRec(first, pivotIndex - 1);
+        quickSortRec(pivotIndex + 1, last);
+    }
+
+
+    /**
+     * Operates on the subarray of points[] with indices between first and last.
+     *
+     * @param first
+     * @param last
+     * @return
+     */
+    private int partition(int first, int last)
+    {
+        Point pivot = points[last];
+        int i = first - 1;
+
+        for(int j = first; j < last; j++){
+            if(pointComparator.compare(points[j], pivot) <= 0){
+                i++;
+                swap(j,i);
+            }
+        }
+
+        swap(i + 1, last);
+        return i + 1;
+    }
+
+
+
+
+    // Other private methods if needed ...
 }
